e25869a948c94224a10853c9598bb2af5b588163
Aisyah Nabila <150516417+xxsyahna@users.noreply.github.com>
2026-08-19 10:08:37 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Katalog-Ruang-Koleksi-Langka/',
-  https://script.google.com/macros/s/AKfycbz2CkeefUhUbiVdIJLNjZfq7PYfQlPtlJ6x_SUEPGrqHYFaPs6uUTKsQ_HQtHIB459p/exec
 })

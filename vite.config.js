74468ecfa46b63816b7f3666b8b33bc38c73c8db
ruanglangka <ruanglangkagtp@gmail.com>
2026-08-19 09:55:41 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/KATALOG-RUANG-KOLEKSI-LANGKA/',
+  base: '/Katalog-Ruang-Koleksi-Langka/',
 })
